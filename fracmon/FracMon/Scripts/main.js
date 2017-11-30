29cc8942439ae598c96c64b9560a8e7640687367
melsaban <melsaban@raisaenergy.com>
2017-11-30 09:35:31 -0600

    first commit

---
+++ fracmon/FracMon/Scripts/main.js
@@ -0,0 +1,117 @@
+﻿var wellsList = null;  // List of WellDTO
+var currentWell = null; // ID of the current selected well
+var currentPoint = null; // ID of the current data point
+var currentStage = null; // Current Stage #
+var stagesList = null; // list of stages for the current well
+
+function loadWellsList() {
+    wellsList = ajaxCall("GET", null, "/wells/");
+    console.log(wellsList);
+    showWells();
+}
+
+function showWells() {
+    $('#wellsList')
+    .find('option')
+    .remove()
+    .end()
+    .append('<option value="-1">Select A Well</option>');
+    //.val('whatever')
+
+    for (var i = 0; i < wellsList.length; i++) {
+        //$('#wellsList').append($('', { value: wellsList[i].Id, text: wellsList[i].Name }));
+        $('#wellsList').append($("<option></option>")
+                    .attr("value", wellsList[i].Id)
+                    .text(wellsList[i].Name));
+    }
+}
+
+function wellSelected() {
+    var e = document.getElementById("wellsList");
+    selectWell(e.options[e.selectedIndex].value);
+}
+
+function selectWell(wellId) {
+    currentWell = wellId;
+    stagesList = ajaxCall("GET", null, "/wells/" + currentWell +"/stages");
+    console.log(stagesList);
+
+    currentPoint = 0;
+
+    var dps = []; // dataPoints
+    var chart = new CanvasJS.Chart("stageChart1", {
+        title: {
+            text: "Dynamic Data"
+        },
+        axisY: {
+            includeZero: false
+        },
+        data: [{
+            type: "line",
+            dataPoints: dps
+        }]
+    });
+
+    var xVal = 0;
+    var yVal = 100;
+    var updateInterval = 1000;
+    var dataLength = 20; // number of dataPoints visible at any point
+    var intervalId = null;
+    var updateChart = function (count) {
+
+        count = count || 1;
+
+        for (var j = 0; j < count; j++) {
+            yVal = yVal + Math.round(5 + Math.random() * (-5 - 5));
+
+            dps.push({
+                x: xVal,
+                y: yVal
+            });
+            xVal++;
+        }
+
+        if (dps.length > dataLength) {
+            dps.shift();
+        }
+
+        chart.render();
+    };
+
+    var updateChartFromServer = function () {
+        var currentWellPoint = ajaxCall("GET", null, "/wells/" + currentWell + "/" + currentPoint);
+
+        if (currentWellPoint == null) {
+            clearInterval(intervalId);
+        } else {
+            currentPoint = currentWellPoint.Id;
+            if (currentWellPoint.Stage == -1) {
+            } else if (currentWellPoint.Stage != currentStage) {
+                if (currentStage != null)
+                    dps.splice(0, dps.length);
+
+                currentStage = currentWellPoint.Stage;
+            }
+
+            dps.push({
+                x: currentPoint,
+                y: currentWellPoint.Pressure
+            });
+
+            /*
+            if (dps.length > dataLength) {
+                dps.shift();
+            }
+            */
+            chart.render();
+        }
+
+    };
+
+    //updateChart(dataLength);
+    //setInterval(function () { updateChart() }, updateInterval);
+
+    updateChartFromServer();
+    intervalId = setInterval(function () { updateChartFromServer() }, updateInterval);
+
+}
---
+++ fracmon/FracMon/Scripts/tabs.js
@@ -0,0 +1,50 @@
+﻿function loadTabs() {
+    jQuery('ul.tabs').each(function () {
+        // For each set of tabs, we want to keep track of
+        // which tab is active and it's associated content
+        var $active, $content, $links = jQuery(this).find('a');
+
+        // If the location.hash matches one of the links, use that as the active tab.
+        // If no match is found, use the first link as the initial active tab.
+        $active = jQuery($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
+        $active.addClass('active');
+
+        $content = $($active[0].hash);
+
+        // Hide the remaining content
+        $links.not($active).each(function () {
+            jQuery(this.hash).hide();
+        });
+
+        // Bind the click event handler
+        jQuery(this).on('click', 'a', function (e) {
+            // Make the old tab inactive.
+            $active.removeClass('active');
+            $content.hide();
+
+            // Update the variables with the new link and content
+            $active = jQuery(this);
+            $content = jQuery(this.hash);
+
+            // Make the tab active.
+            $active.addClass('active');
+            $content.show();
+
+            // Prevent the anchor's default click action
+            e.preventDefault();
+        });
+    });
+}
+
+function addStageChartTab(stageNumber) {
+    var tabs = $("#stageTabContainer").tabs();
+    var ul = tabs.find("ul");
+
+    var stageName = "stage" + stageNumber;
+    var chartName = "stageChart" + stageNumber;
+    var tabLink = '<li><a href="#' + stageName + '">Stage ' + stageNumber + '</a></li>';
+    var tabContent = '<div id="' +stageName +'"><div id="' +chartName +'" style="height: 370px; width:100%;"></div></div>';
+    $(tabLink).appendTo(ul);
+    $(tabContent).appendTo(tabs);
+    tabs.tabs("refresh");
+}
